31518db603f55dfd87d8498710a2872b6b49b7c6
qianwu2 <qianwu2@ap.xom.com>
2020-01-16 22:13:42 +0800

    newMarsRover [qian]do some refactor for direction that move function from enum to marsRover

--- src/main/java/first/FirstDirection.java
+++ src/main/java/first/FirstDirection.java
@@ -1,28 +1,8 @@
 package first;
 
-import java.util.function.Function;
-
 public enum FirstDirection {
-  NORTH(Constants.ADD_Y_FUNCTION),
-  EAST(Constants.ADD_X_FUNCTION),
-  SOUTH(Constants.SUB_Y_FUNCTION),
-  WEST(Constants.SUB_X_FUNCTION);
-
-  private Function<FirstMarsRover, FirstMarsRover> goForward;
-
-  public Function<FirstMarsRover, FirstMarsRover> getGoForward() {
-    return goForward;
-  }
-
-  FirstDirection(Function<FirstMarsRover, FirstMarsRover> goForward) {
-    this.goForward = goForward;
-  }
-
-  private static class Constants {
-
-    static final Function<FirstMarsRover, FirstMarsRover> SUB_Y_FUNCTION = marsOVer -> new FirstMarsRover(marsOVer.getX(), marsOVer.getY() - 1, marsOVer.getDirection());
-    static final Function<FirstMarsRover, FirstMarsRover> SUB_X_FUNCTION = marsOVer -> new FirstMarsRover(marsOVer.getX() - 1, marsOVer.getY(), marsOVer.getDirection());
-    static final Function<FirstMarsRover, FirstMarsRover> ADD_X_FUNCTION = marsOVer -> new FirstMarsRover(marsOVer.getX() + 1, marsOVer.getY(), marsOVer.getDirection());
-    static final Function<FirstMarsRover, FirstMarsRover> ADD_Y_FUNCTION = marsOVer -> new FirstMarsRover(marsOVer.getX(), marsOVer.getY() + 1, marsOVer.getDirection());
-  }
+  NORTH(),
+  EAST(),
+  SOUTH(),
+  WEST()
 }
--- src/main/java/first/FirstMarsRover.java
+++ src/main/java/first/FirstMarsRover.java
@@ -1,7 +1,14 @@
 package first;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+import static first.FirstDirection.EAST;
+import static first.FirstDirection.NORTH;
+import static first.FirstDirection.SOUTH;
+import static first.FirstDirection.WEST;
 import static first.FirstDirection.values;
 
 public class FirstMarsRover {
@@ -9,6 +16,18 @@ public class FirstMarsRover {
   private int x;
   private int y;
   private FirstDirection direction;
+
+  private final Function<FirstMarsRover, FirstMarsRover> SUB_Y_FUNCTION = marsRover -> {marsRover.y--; return marsRover;};
+  private final Function<FirstMarsRover, FirstMarsRover> SUB_X_FUNCTION = marsRover -> {marsRover.x--; return marsRover;};
+  private final Function<FirstMarsRover, FirstMarsRover> ADD_X_FUNCTION = marsRover -> {marsRover.x++; return marsRover;};
+  private final Function<FirstMarsRover, FirstMarsRover> ADD_Y_FUNCTION = marsRover -> {marsRover.y++; return marsRover;};
+  private Map<FirstDirection, Function<FirstMarsRover, FirstMarsRover>> functionMap = new HashMap<FirstDirection, Function<FirstMarsRover, FirstMarsRover>>() {{
+    put(NORTH, ADD_Y_FUNCTION);
+    put(EAST, ADD_X_FUNCTION);
+    put(SOUTH, SUB_Y_FUNCTION);
+    put(WEST, SUB_X_FUNCTION);
+
+  }};
 
   public FirstMarsRover(int x, int y, FirstDirection direction) {
 
@@ -33,17 +52,19 @@ public class FirstMarsRover {
 
     int originalIndex = Arrays.asList(values()).indexOf(direction);
     int indexOfLeft = originalIndex == 0 ? 3 : originalIndex - 1;
-    return new FirstMarsRover(x, y, values()[indexOfLeft]);
+    this.direction = values()[indexOfLeft];
+    return this;
   }
 
   public FirstMarsRover turnRight() {
     int originalIndex = Arrays.asList(values()).indexOf(direction);
     int indexOfRight = originalIndex == 3 ? 0 : originalIndex + 1;
-    return new FirstMarsRover(x, y, values()[indexOfRight]);
+    this.direction = values()[indexOfRight];
+    return this;
   }
 
   public FirstMarsRover goForward() {
-    return direction.getGoForward().apply(this);
+    return functionMap.get(direction).apply(this);
   }
 
 }
